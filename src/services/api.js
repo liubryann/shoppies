1a1aa582f2c3fe1086b700a832235da056c47140
Bryan Liu <liubryan.developer@gmail.com>
2021-01-12 02:11:14 -0500

    fix(api.js): remove secret env variable for api key because it's exposed in the url anyways

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
-import OMDB_API_KEY from '../config';
 const OMDB_URL = "http://www.omdbapi.com/";
-
-if (process.env.NODE_ENV === 'production') {
-    OMDB_API_KEY = process.env.OMDB_API_KEY;
-}
+const OMDB_API_KEY = "a7b58694";
 
 const client = axios.create({
     baseURL: OMDB_URL,
